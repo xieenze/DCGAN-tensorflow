b10e8c8b73448300570cdf6470d1b793dabf78a1
谢恩泽 <Johnny_ez@163.com>
2017-10-22 10:38:13 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -192,7 +192,7 @@ def visualize(sess, dcgan, config, option):
       else:
         samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
 
-      save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_arange_%s.png' % (idx))
+      save_images(samples, [image_frame_dim, image_frame_dim], './test_samples/test_arange_%s.png' % (idx))
   elif option == 2:
     values = np.arange(0, 1, 1./config.batch_size)
     for idx in [random.randint(0, 99) for _ in xrange(100)]:
